a0389cee1d5c1020c37c1a4a309a149ef0c53b65
Mhmd <mhmd.tarek197@gmail.com>
2022-10-23 02:44:37 +0200

    ADD: base styles setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
   },
   plugins: [],
 };
