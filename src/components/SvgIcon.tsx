8507f0a2e72572bb8150c1de97b9fb53b7500861
Mhmd <mhmd.tarek197@gmail.com>
2022-10-23 18:44:29 +0200

    EDIT: SvgIcon component

--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -2,8 +2,6 @@ interface Svg {
   children: JSX.Element | JSX.Element[];
   viewBox?: string;
   className?: string;
-  width?: string;
-  height?: string;
 }
 
 function SvgIcon({ children, ...props }: Svg): JSX.Element {
@@ -11,6 +9,9 @@ function SvgIcon({ children, ...props }: Svg): JSX.Element {
     <svg
       aria-hidden="true"
       focusable="false"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
